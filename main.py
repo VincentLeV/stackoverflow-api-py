fc570e01046af158b866736f3947c8b05a0a1e20
Vincent Le <vincentle.fh@gmail.com>
2025-04-11 15:19:18 +0300

    Better explanation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from scraper import extract_data
-# from data.stackoverflow_python import questions as py
-# from data.stackoverflow_javascript import questions as js
 
 app = FastAPI()
 
@@ -22,21 +20,17 @@ async def root():
         "Information": {
             "API URL Prefix": "/api",
             "Endpoints": "Any available stackoverflow tag should work",
+            "Examples": {
+                "Python": "/api/python",
+                "JavaScript": "/api/javascript",
+                "ReactJS": "/api/reactjs",
+                "C#": "/api/csharp",
+                "C++": "/api/c++",
+                "Go": "/api/go",
+            },
             "Timestamp": "GMT+2 Timezone"
         }
     }
-
-
-# @app.get("/api/python")
-# def get_py_questions():
-#     data = extract_data(tags=["python"], max_pages=1)
-#     return data
-#
-#
-# @app.get("/api/javascript")
-# def get_js_questions():
-#     data = extract_data(tags=["javascript"], max_pages=1)
-#     return data
 
 @app.get("/api/{language}")
 async def get_questions(language: str):
